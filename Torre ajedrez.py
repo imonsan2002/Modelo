5f363f31076982f17cb91cc8261115601ae9a7ed
imonsan2002 <33682856+imonsan2002@users.noreply.github.com>
2017-11-22 09:16:48 +0100

    Add files via upload

---
+++ Torre ajedrez.py
@@ -0,0 +1,10 @@
+columna = int(input("columna"))
+fila = int(input("fila"))
+columna2 = int(input("columna2"))
+fila2 = int(input("fila2"))
+if columna == columna2:
+  print("si")
+elif fila == fila2:
+  print("si")
+else:
+  print("no")
