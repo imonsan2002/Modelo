ef52b38e60d0316809b9547f23fd4749c056c492
imonsan2002 <33682856+imonsan2002@users.noreply.github.com>
2018-01-17 09:04:02 +0100

    Add files via upload

---
+++ P2 series 2.py
@@ -0,0 +1,15 @@
+n1 = int(input("numero 1? ="))
+
+n2 = int(input("numero 2? ="))
+
+if n1 > n2:
+
+  for z in range(n1,n2 - 1, -1):
+
+    print(z)
+
+elif n2 > n1:
+
+  for y in range(n1,n2 + 1):
+
+    print(y)
